3da2747aa82ec11ef6a4b36a7c2166d60edefd10
NihaalNijjar <153221375+NihaalNijjar@users.noreply.github.com>
2024-01-12 18:37:55 -0500

    camera movement, and mouse input

--- src/files/main.java
+++ src/files/main.java
@@ -5,30 +5,40 @@ import java.awt.*;
 import javax.sound.sampled.Clip;
 
 public class main {
-	
+
 	public static void main(String[] args) throws InterruptedException {
 
 		new main();
-		
 
 	}
 
-	static GraphicsConsole gc = new GraphicsConsole(26 * 12, 47 * 12, "Tomb of the Mask");
+	static int mapX = 36 * 12;
+	static int mapY = 48 * 12;
+	static GraphicsConsole gc = new GraphicsConsole(mapX, mapY, "Tomb of the Mask");
 
 	main() throws InterruptedException {
 
 		gc.setLocationRelativeTo(null);
 
+		int lvly = 75;
+		int lvlw = 60;
 		int pixel = 12;
 
 		int delay = 15;
 		int ticks = 0;
 		int seconds = 0;
 
+		int MARGIN = 5 * pixel;
+		int XBLOCKS = 26;
+		int YBLOCKS = 46;
+		int xoffset = 0;
+		int yoffset = 0;
+
 		Boolean Start = false;
 		Boolean Finish = true;
 		Boolean Dead = false;
 		Boolean Shield = false;
+		Boolean DrawMask = false;
 		int Life = 1;
 
 		int Maskx = 0;
@@ -38,6 +48,11 @@ public class main {
 		int Star = 0;
 		int Coin = 0;
 		int Balance = 0;
+
+		int mouseclick = 0;
+		gc.enableMouse();
+		gc.enableMouseMotion();
+		gc.enableMouseWheel();
 
 		Boolean Audio = true;
 
@@ -50,10 +65,9 @@ public class main {
 		int Lavay = 650;
 
 		gc.setBackgroundColor(Color.BLACK);
-		
-		
+
 		stages maps = new stages();
-		int[][] map = maps.map3;
+		int[][] map = maps.map1;
 
 		if (map == maps.map1) {
 			Maskx = 17;
@@ -90,50 +104,57 @@ public class main {
 						for (int col = 0; col < map[0].length; col++) {
 							switch (map[row][col]) {
 							case 1:
-								gc.drawImage(assets.dot, col * pixel, row * pixel, pixel, pixel);
+								gc.drawImage(assets.dot, col * pixel - xoffset, row * pixel - yoffset, pixel, pixel);
 								break;
 							case 2:
-								gc.drawImage(assets.coin, col * pixel, row * pixel, pixel, pixel);
+								gc.drawImage(assets.coin, col * pixel - xoffset, row * pixel - yoffset, pixel, pixel);
 								break;
 							case 3:
-								gc.drawImage(assets.star, col * pixel, row * pixel, pixel, pixel);
+								gc.drawImage(assets.star, col * pixel - xoffset, row * pixel - yoffset, pixel, pixel);
 								break;
 							case 4:
-								gc.drawImage(assets.exit, col * pixel, row * pixel, pixel, pixel);
+								gc.drawImage(assets.exit, col * pixel - xoffset, row * pixel - yoffset, pixel, pixel);
 								break;
 							case 5:
-								gc.drawImage(assets.spike, col * pixel, row * pixel, pixel, pixel);
+								gc.drawImage(assets.spike, col * pixel - xoffset, row * pixel - yoffset, pixel, pixel);
 								break;
 							case 6:
-								gc.drawImage(assets.spiketrap, col * pixel, row * pixel, pixel, pixel);
+								gc.drawImage(assets.spiketrap, col * pixel - xoffset, row * pixel - yoffset, pixel,
+										pixel);
 								break;
 							// case 7: gc.drawImage(spikeactive, col * pixel, row * pixel, pixel, pixel);
 							// break;
 							// case 8: gc.drawImage(bat, col * pixel, row * pixel, pixel, pixel); break;
 							// break;
+
 							case 9:
 								if (map == maps.map6)
-									gc.drawImage(assets.wall2, col * pixel, row * pixel, pixel, pixel);
+									gc.drawImage(assets.wall2, col * pixel - xoffset, row * pixel - yoffset, pixel,
+											pixel);
+
 								else
-									gc.drawImage(assets.wall1, col * pixel, row * pixel, pixel, pixel);
+									gc.drawImage(assets.wall1, col * pixel - xoffset, row * pixel - yoffset, pixel,
+											pixel);
+								break;
+
+							case 10:
+								gc.setColor(Color.WHITE);
+								gc.fillRect(col * pixel - xoffset, row * pixel - yoffset, lvly, lvlw);
 								break;
 							}
 						}
 					}
 
 					// gc.drawImage(entrance,Maskx * pixel, Masky * pixel);
-					gc.drawImage(assets.mask, Maskx * pixel, Masky * pixel, pixel, pixel);
-
-					if (Lava) {
-						if (ticks % 20 == 0) {
-							gc.setColor(Color.CYAN);
-						} else if (ticks % 10 == 0) {
-							gc.setColor(Color.MAGENTA);
-						}
-						gc.fillRect(0, Lavay, 1000, 750);
-						if (ticks % 2 == 0)
-							Lavay -= LavaSpeed;
+					if (DrawMask == true) {
+						gc.drawImage(assets.mask, Maskx * pixel - xoffset, Masky * pixel - yoffset, pixel, pixel);
 					}
+
+					/*
+					 * if (Lava) { if (ticks % 20 == 0) { gc.setColor(Color.CYAN); } else if (ticks
+					 * % 10 == 0) { gc.setColor(Color.MAGENTA); } gc.fillRect(0, Lavay, 1000, 750);
+					 * if (ticks % 2 == 0) Lavay -= LavaSpeed; }
+					 */
 
 					if (Finish) {
 						break;
@@ -148,171 +169,256 @@ public class main {
 				}
 
 				Thread.sleep(delay);
+				if (map != maps.start) {
+					// User Input
+					if (Move == false) {
+						if (gc.isKeyDown('W') || gc.isKeyDown(GraphicsConsole.VK_UP)) {
+							MaskD = 1;
+							System.out.println("move");
+						} else if (gc.isKeyDown('A') || gc.isKeyDown(GraphicsConsole.VK_LEFT)) {
+							MaskD = 2;
+						} else if (gc.isKeyDown('S') || gc.isKeyDown(GraphicsConsole.VK_DOWN)) {
+							MaskD = 3;
+						} else if (gc.isKeyDown('D') || gc.isKeyDown(GraphicsConsole.VK_RIGHT)) {
+							MaskD = 4;
+						}
+					}
+					// Enter to buy shield
+					if (gc.isKeyDown(GraphicsConsole.VK_ENTER)) {
+						if (Life == 1 && (Balance >= 100) && Shield == false) {
+							Life++;
+							// System.out.println("SHIELD ++ " + Life);
+							Balance -= 100;
+							Shield = true;
+						}
+					}
+					// ESC to pause
+					if (gc.isKeyDown(GraphicsConsole.VK_ESCAPE)) {
+						// M to mute
+						if (gc.isKeyDown('M')) {
+							Audio = false;
+						}
+					}
 
-				// User Input
-				if (Move == false) {
-					if (gc.isKeyDown('W') || gc.isKeyDown(GraphicsConsole.VK_UP)) {
-						MaskD = 1;
-					} else if (gc.isKeyDown('A') || gc.isKeyDown(GraphicsConsole.VK_LEFT)) {
-						MaskD = 2;
-					} else if (gc.isKeyDown('S') || gc.isKeyDown(GraphicsConsole.VK_DOWN)) {
-						MaskD = 3;
-					} else if (gc.isKeyDown('D') || gc.isKeyDown(GraphicsConsole.VK_RIGHT)) {
-						MaskD = 4;
+					// Updates
+					if (Move) {
+						switch (MaskD) {
+						case 0:
+						case 1:
+							Masky--;
+							break;
+						case 2:
+							Maskx--;
+							break;
+						case 3:
+							Masky++;
+							break;
+						case 4:
+							Maskx++;
+							break;
+						}
 					}
-				}
-				// Enter to buy shield
-				if (gc.isKeyDown(GraphicsConsole.VK_ENTER)) {
-					if (Life == 1 && (Balance >= 100) && Shield == false) {
-						Life++;
-						System.out.println("SHIELD ++ " + Life);
-						Balance -= 100;
-						Shield = true;
-					}
-				}
-				// ESC to pause
-				if (gc.isKeyDown(GraphicsConsole.VK_ESCAPE)) {
-					// M to mute
-					if (gc.isKeyDown('M')) {
-						Audio = false;
-					}
-				}
 
-				// Updates
-				if (Move) {
-					switch (MaskD) {
-					case 0: 
-					case 1:
-						Masky--;
+					ticks++;
+					if (true) {
+						seconds++;
+						// System.out.println("-------------------------" + seconds);
+						// System.out.println("xoffset: " + xoffset);
+						// System.out.println("yoffset: " + yoffset);
+					}
+
+					// Collision Detection
+
+					if (map[Masky][Maskx] == 1) {
+						map[Masky][Maskx] = 0;
+						Balance++;
+						Dot++;
+					} else if (map[Masky][Maskx] == 2) {
+						map[Masky][Maskx] = 0;
+						Balance += 5;
+						Coin++;
+						if (Audio)
+							gc.playSound(assets.scoin);
+					} else if (map[Masky][Maskx] == 3) {
+						map[Masky][Maskx] = 0;
+						Balance += 10;
+						Star++;
+						if (Audio)
+							gc.playSound(assets.sstar);
+					} else if (map[Masky][Maskx] == 4) {
+						Finish = true;
+						map[Masky][Maskx] = 0;
+						if (Audio)
+							gc.playSound(assets.sexit);
 						break;
-					case 2:
-						Maskx--;
-						break;
-					case 3:
-						Masky++;
-						break;
-					case 4:
-						Maskx++;
-						break;
+					}
+
+					if (Lavay <= (Masky * pixel)) {
+						Life--;
+					}
+
+					if (MaskD == 1) {
+						if (map[Masky - 1][Maskx] != 9 && map[Masky - 1][Maskx] != 6 && map[Masky - 1][Maskx] != 5) {
+							Move = true;
+						} else if (map[Masky - 1][Maskx] == 5 || map[Masky - 1][Maskx] == 7) {
+							Life--;
+							Shield = false;
+							MaskD = 0;
+						} else {
+							Move = false;
+						}
+					} else if (MaskD == 2) {
+						if (map[Masky][Maskx - 1] != 9 && map[Masky][Maskx - 1] != 6 && map[Masky][Maskx - 1] != 5) {
+							Move = true;
+						} else if (map[Masky][Maskx - 1] == 5 || map[Masky][Maskx - 1] == 7) {
+							Life--;
+							Shield = false;
+							MaskD = 0;
+						} else {
+							Move = false;
+						}
+					} else if (MaskD == 3) {
+						if (map[Masky + 1][Maskx] != 9 && map[Masky + 1][Maskx] != 6 && map[Masky + 1][Maskx] != 5) {
+							Move = true;
+						} else if ((map[Masky + 1][Maskx] == 5 || map[Masky + 1][Maskx] == 7)) {
+							Life--;
+							Shield = false;
+							MaskD = 0;
+						} else {
+							Move = false;
+						}
+					} else if (MaskD == 4) {
+						if (map[Masky][Maskx + 1] != 9 && map[Masky][Maskx + 1] != 6 && map[Masky][Maskx + 1] != 5) {
+							Move = true;
+						} else if ((map[Masky][Maskx + 1] == 5 || map[Masky][Maskx + 1] == 7)) {
+							Life--;
+							Shield = false;
+							MaskD = 0;
+						} else {
+							Move = false;
+						}
+					}
+
+					// Scroll the screen up/down/left/right to keep the character on-screen
+
+					int xposition = Maskx * pixel;
+					int yposition = Masky * pixel;
+					if (xoffset > xposition - MARGIN) {
+						xoffset = xposition - MARGIN;
+						if (xoffset < 0)
+							xoffset = 0;
+					}
+
+					if (xoffset < xposition - mapX + MARGIN + pixel) {
+						xoffset = xposition - mapX + MARGIN + pixel;
+						if (xoffset > XBLOCKS * pixel - mapX)
+							xoffset = XBLOCKS * pixel - mapX;
+					}
+
+					if (yoffset > yposition - MARGIN) {
+						yoffset = yposition - MARGIN;
+						if (yoffset < 0)
+							yoffset = 0;
+					}
+
+					if (yoffset < yposition - mapY + MARGIN + pixel) {
+						yoffset = yposition - mapY + MARGIN + pixel;
+						if (yoffset > YBLOCKS * pixel - mapY)
+							yoffset = YBLOCKS * pixel - mapY;
+					}
+				}
+				gc.setCursor(10, 10);
+				mouseclick = gc.getMouseClick();
+
+				if ((mouseclick != 0) && (4 * pixel < gc.getMouseX()) && (gc.getMouseX() < 5 * pixel + lvlw)
+						&& (13 * pixel < gc.getMouseY()) && (gc.getMouseY() < 13 * pixel + lvly)) {
+					// if (mouseclick != 0) {
+					System.out.printf("location is" + mouseclick);
+					map = maps.map1;
+					DrawMask = true;
+					if (map == maps.map1) {
+						Maskx = 17;
+						Masky = 36;
+					}
+
+				} else if ((mouseclick != 0) && (4 * pixel < gc.getMouseX()) && (gc.getMouseX() < 5 * pixel + lvlw)
+						&& (23 * pixel < gc.getMouseY()) && (gc.getMouseY() < 23 * pixel + lvly)) {
+					System.out.printf("location is" + mouseclick);
+					map = maps.map2;
+					DrawMask = true;
+					if (map == maps.map2) {
+						Maskx = 14;
+						Masky = 16;
+					}
+				} else if ((mouseclick != 0) && (15 * pixel < gc.getMouseX()) && (gc.getMouseX() < 16 * pixel + lvlw)
+						&& (23 * pixel < gc.getMouseY()) && (gc.getMouseY() < 23 * pixel + lvly)) {
+					System.out.printf("location is" + mouseclick);
+					map = maps.map3;
+					DrawMask = true;
+					if (map == maps.map3) {
+						Maskx = 5;
+						Masky = 20;
+					}
+				} else if ((mouseclick != 0) && (15 * pixel < gc.getMouseX()) && (gc.getMouseX() < 16 * pixel + lvlw)
+						&& (13 * pixel < gc.getMouseY()) && (gc.getMouseY() < 13 * pixel + lvly)) {
+					System.out.printf("location is" + mouseclick);
+					map = maps.map4;
+					DrawMask = true;
+					if (map == maps.map4) {
+						Maskx = 5;
+						Masky = 44;
+					}
+				} else if ((mouseclick != 0) && (26 * pixel < gc.getMouseX()) && (gc.getMouseX() < 27 * pixel + lvlw)
+						&& (13 * pixel < gc.getMouseY()) && (gc.getMouseY() < 13 * pixel + lvly)) {
+					System.out.printf("location is" + mouseclick);
+					map = maps.map5;
+					DrawMask = true;
+					if (map == maps.map5) {
+						Maskx = 19;
+						Masky = 34;
+					}
+				} else if ((mouseclick != 0) && (26 * pixel < gc.getMouseX()) && (gc.getMouseX() < 27 * pixel + lvlw)
+						&& (23 * pixel < gc.getMouseY()) && (gc.getMouseY() < 23 * pixel + lvly)) {
+					System.out.printf("location is" + mouseclick);
+					map = maps.map6;
+					DrawMask = true;
+					if (map == maps.map6) {
+						Maskx = 19;
+						Masky = 35;
 					}
 				}
 
-				ticks++;
-				if (ticks % (1000 / delay) == 0) {
-					seconds++;
-					System.out.println("-------------------------" + seconds);
-					System.out.println("X: " + Maskx);
-					System.out.println("Y: " + Masky);
-					System.out.println("$: " + Balance);
-					System.out.println("L: " + Life);
-				}
-
-				// Collision Detection
-				if (map[Masky][Maskx] == 1) {
-					map[Masky][Maskx] = 0;
-					Balance ++;
-					Dot++;
-				} else if (map[Masky][Maskx] == 2) {
-					map[Masky][Maskx] = 0;
-					Balance += 5;
-					Coin++;
-					if (Audio)
-						gc.playSound(assets.scoin);
-				} else if (map[Masky][Maskx] == 3) {
-					map[Masky][Maskx] = 0;
-					Balance += 10;
-					Star++;
-					if (Audio)
-						gc.playSound(assets.sstar);
-				} else if (map[Masky][Maskx] == 4) {
-					Finish = true;
-					map[Masky][Maskx] = 0;
-					if (Audio)
-						gc.playSound(assets.sexit);
-				}
-
-				if (Lavay <= (Masky * pixel)) {
-					Life--;
-				}
-
-				if (MaskD == 1) {
-					if (map[Masky - 1][Maskx] != 9 && map[Masky - 1][Maskx] != 6 && map[Masky - 1][Maskx] != 5) {
-						Move = true;
-					} else if (map[Masky - 1][Maskx] == 5 || map[Masky - 1][Maskx] == 7) {
-						Life--;
-						Shield = false;
-						MaskD = 0;
-					} else {
-						Move = false;
-					}
-				} else if (MaskD == 2) {
-					if (map[Masky][Maskx - 1] != 9 && map[Masky][Maskx - 1] != 6 && map[Masky][Maskx - 1] != 5) {
-						Move = true;
-					} else if (map[Masky][Maskx - 1] == 5 || map[Masky][Maskx - 1] == 7) {
-						Life--;
-						Shield = false;
-						MaskD = 0;
-					} else {
-						Move = false;
-					}
-				} else if (MaskD == 3) {
-					if (map[Masky + 1][Maskx] != 9 && map[Masky + 1][Maskx] != 6 && map[Masky + 1][Maskx] != 5) {
-						Move = true;
-					} else if ((map[Masky + 1][Maskx] == 5 || map[Masky + 1][Maskx] == 7)) {
-						Life--;
-						Shield = false;
-						MaskD = 0;
-					} else {
-						Move = false;
-					}
-				} else if (MaskD == 4) {
-					if (map[Masky][Maskx + 1] != 9 && map[Masky][Maskx + 1] != 6 && map[Masky][Maskx + 1] != 5) {
-						Move = true;
-					} else if ((map[Masky][Maskx + 1] == 5 || map[Masky][Maskx + 1] == 7)) {
-						Life--;
-						Shield = false;
-						MaskD = 0;
-					} else {
-						Move = false;
-					}
-				}
 			}
-
 			while (Finish) {
+				MaskD = 0;
+				Move = false;
+				DrawMask = false;
 				// Map Transition (for tester)
 				if (map == maps.map1) {
-					Maskx = 14;
-					Masky = 16;
-					map = maps.map2;
-					System.out.println("Stage2---------------------");
+					map = maps.start;
+					// System.out.println("Stage2---------------------");
 				} else if (map == maps.map2) {
-					Maskx = 5;
-					Masky = 20;
-					map = maps.map3;
-					System.out.println("Stage3---------------------");
+					map = maps.start;
+					// System.out.println("Stage3---------------------");
 				} else if (map == maps.map3) {
-					Maskx = 5;
-					Masky = 44;
-					map = maps.map4;
+					map = maps.start;
 					Lava = true;
-					System.out.println("Stage4---------------------");
+					// System.out.println("Stage4---------------------");
 				} else if (map == maps.map4) {
-					Maskx = 19;
-					Masky = 34;
-					map = maps.map5;
+					map = maps.start;
 					Lava = false;
 					Lavay = 650;
-					System.out.println("Stage5---------------------");
+					// System.out.println("Stage5---------------------");
 				} else if (map == maps.map5) {
-					Maskx = 19;
-					Masky = 35;
-					map = maps.map6;
-					System.out.println("Stage6---------------------");
+					map = maps.start;
+
+					// System.out.println("Stage6---------------------");
 				} else if (map == maps.map6) {
-					Maskx = 17;
-					Masky = 36;
-					map = maps.map1;
-					System.out.println("Stage1---------------------");
+					map = maps.start;
+
+					// System.out.println("Stage1---------------------");
+				} else if (map == maps.start) {
+
 				}
 				Finish = false;
 				Start = true;
@@ -324,6 +430,7 @@ public class main {
 				// gc.getChar();
 				break;
 			}
+
 		}
 
 	}
