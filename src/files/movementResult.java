3bdc5871da303a7782f108b2922f8642c3f545a9
NihaalNijjar <153221375+NihaalNijjar@users.noreply.github.com>
2024-01-29 18:20:46 -0500

    Add files via upload

---
+++ src/files/movementResult.java
@@ -0,0 +1,17 @@
+//Tei and Nihaal
+//Tomb of the mask final project
+//January 19, 2024
+//This class file
+
+package files;
+
+//Hold the changes to movement so that it can be provided to the main method
+public class movementResult {
+	
+	boolean Move;
+	boolean SFX;
+	boolean Shield;
+	int Life;
+	int MaskD;
+	
+}
